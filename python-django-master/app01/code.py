0df292d23702eb984be558c92f9559309f449bb1
nhlsb <116778824+nhlsb@users.noreply.github.com>
2023-06-05 08:56:29 +0800

    code.py

--- python-django-master/app01/code.py
+++ python-django-master/app01/code.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 import random
 
-
 def check_code(width=120, height=30, char_length=5, font_file='Monaco.ttf', font_size=28):
     code = []
     img = Image.new(mode='RGB', size=(width, height), color=(255, 255, 255))
